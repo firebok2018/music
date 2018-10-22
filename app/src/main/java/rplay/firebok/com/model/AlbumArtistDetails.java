29029ca69716606b07c1dbd9a9d6feb7dbde628b
usrcdiar <usrcdiar@cibertec.edu.pe>
2018-10-22 15:28:30 -0500

    tab-boorar codigo inservible

---
+++ app/src/main/java/rplay/firebok/com/model/AlbumArtistDetails.java
@@ -0,0 +1,12 @@
+
+package rplay.firebok.com.model;
+
+/**
+ * Simple containing class to query for album art
+ */
+public class AlbumArtistDetails {
+    public long mAudioId;
+    public long mAlbumId;
+    public String mAlbumName;
+    public String mArtistName;
+}
